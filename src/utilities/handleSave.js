a89a3267093c0f2a83276eb7c172c711b59a3aff
Benjamin Malberg <bmalberg@protonmail.com>
2022-12-31 16:29:58 -0800

    fixed handleSave.js location and cleaned up

--- src/utilities/handleSave.js
+++ src/utilities/handleSave.js
@@ -1,11 +1,16 @@
 import { saveAs } from "file-saver";
 
 export const handleSave = (entries, currentId, size) => {
-    
     if (size === 1) {
         const current = entries.find((entry) => entry.id === currentId);
         const currentEntry = current.body;
-        const currentTimeStamp = current.timeStamp.replaceAll(" ", "");
+
+        // checks for old version which didn't include timestamp property
+        const newVersionConfirmation = current.hasOwnProperty("timeStamp");
+
+        const currentTimeStamp = newVersionConfirmation
+            ? current.timeStamp.replaceAll(" ", "")
+            : "invalidTimeStamp";
 
         let blob = new Blob([currentEntry], {
             type: "text/plain;charset=utf-8",
@@ -21,15 +26,21 @@ export const handleSave = (entries, currentId, size) => {
             type: "text/plain;charset=utf-8",
         });
 
-        const earliestEntry = entries[
-            allEntries.length - 1
-        ].timeStamp
+        // checks for old version which didn't include timestamp property
+        const newVersionConfirmation =
+            entries[allEntries.length - 1].hasOwnProperty("timeStamp") &&
+            entries[0].hasOwnProperty("timeStamp");
 
-        const latestEntry = entries[0].timeStamp
-        
+        const earliestEntry = newVersionConfirmation
+            ? entries[allEntries.length - 1].timeStamp
+            : "invalidTimeStamp";
+
+        const latestEntry = newVersionConfirmation
+            ? entries[0].timeStamp
+            : "invalidTimeStamp";
+
         const earliestEntryFormat = earliestEntry.replaceAll(" ", "");
         const latestEntryFormat = latestEntry.replaceAll(" ", "");
-        console.log(earliestEntryFormat, latestEntryFormat)
 
         saveAs(blob, `${earliestEntryFormat}-${latestEntryFormat}.txt`);
     }
